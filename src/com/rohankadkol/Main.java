6e4ee5e9e5f2d18daf5aa09ac66925bb5c5f702d
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-09-06 15:06:56 -0500

    Completed Version 2

--- src/com/rohankadkol/Main.java
+++ src/com/rohankadkol/Main.java
@@ -3,10 +3,26 @@ package com.rohankadkol;
 public class Main {
 
     public static void main(String[] args) {
-        int[] sorted = SelectionSort.iterative(new int[]{7, 2, 13, 24, 3, 6, 10});
-        System.out.printf("%d", sorted[0]);
-        for (int i=1; i<sorted.length; i++) {
-            System.out.printf(", %d", sorted[i]);
+        // Notice how the same SelectionSort.iterative() method is being used to sort Integers, Doubles, and Strings.
+
+        Integer[] sortedIntegers = SelectionSort.iterative(new Integer[]{7, 2, 13, 24, 3, 6, 10});
+        System.out.printf("%d", sortedIntegers[0]);
+        for (int i = 1; i < sortedIntegers.length; i++) {
+            System.out.printf(", %d", sortedIntegers[i]);
+        }
+        System.out.println();
+
+        Double[] sortedDoubles = SelectionSort.iterative(new Double[]{7.3, 2.2, 13.5, 24.9, 3.1, 6.0, 10.2});
+        System.out.printf("%.1f", sortedDoubles[0]);
+        for (int i = 1; i < sortedDoubles.length; i++) {
+            System.out.printf(", %.1f", sortedDoubles[i]);
+        }
+        System.out.println();
+
+        String[] sortedStrings = SelectionSort.iterative(new String[]{"Albert", "Steven", "Bernard", "Juliet", "Samantha", "Jessica", "Darnell"});
+        System.out.printf("%s", sortedStrings[0]);
+        for (int i = 1; i < sortedStrings.length; i++) {
+            System.out.printf(", %s", sortedStrings[i]);
         }
     }
 }
--- src/com/rohankadkol/SelectionSort.java
+++ src/com/rohankadkol/SelectionSort.java
@@ -1,15 +1,23 @@
 package com.rohankadkol;
 
 public class SelectionSort {
-    public static int[] iterative(int[] items) {
+    /**
+     * Iterative Selection Sort Algorithm.
+     * Notice how we use a generic T. This is so that the same function can be used to sort all types of Java objects.
+     * Eg. Integers, Doubles, and Strings
+     * @param items items to be sorted
+     * @param <T> Generic type of each of the item in items
+     * @return sorted array of items
+     */
+    public static <T extends Comparable<T>> T[] iterative(T[] items) {
         for (int i = 0; i < items.length - 1; i++) {
             int minIndex = i;
             for (int j = i + 1; j < items.length; j++) {
-                if (items[j] < items[minIndex]) {
+                if (items[j].compareTo(items[minIndex]) < 0) {
                     minIndex = j;
                 }
             }
-            int temp = items[i];
+            T temp = items[i];
             items[i] = items[minIndex];
             items[minIndex] = temp;
         }
