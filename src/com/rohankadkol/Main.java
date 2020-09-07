f1d8f1836589913c45ad2efd7ce5cb9771ab5507
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-09-07 17:15:28 -0500

    Added text for the output arrays.

--- src/com/rohankadkol/Main.java
+++ src/com/rohankadkol/Main.java
@@ -8,17 +8,26 @@ public class Main {
         // Notice how the same SelectionSort.iterative() method is being used to sort Integers, Doubles, and Strings.
 
         Integer[] integers = new Integer[]{7, 2, 13, 24, 3, 6, 10};
+        System.out.println("Before sorting:");
+        System.out.println(Arrays.toString(integers));
         SelectionSort.iterative(integers);
+        System.out.println("After sorting:");
         System.out.println(Arrays.toString(integers));
         System.out.println();
 
         Double[] doubles = new Double[]{7.3, 2.2, 13.5, 24.9, 3.1, 6.0, 10.2};
+        System.out.println("Before sorting:");
+        System.out.println(Arrays.toString(doubles));
         SelectionSort.iterative(doubles);
+        System.out.println("After sorting:");
         System.out.println(Arrays.toString(doubles));
         System.out.println();
 
         String[] strings = new String[]{"Albert", "Steven", "Bernard", "Juliet", "Samantha", "Jessica", "Darnell"};
-        SelectionSort.iterative(doubles);
+        System.out.println("Before sorting:");
+        System.out.println(Arrays.toString(strings));
+        SelectionSort.iterative(strings);
+        System.out.println("After sorting:");
         System.out.println(Arrays.toString(strings));
         System.out.println();
     }
