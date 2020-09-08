22adad78f9a075b2cc8a50b47b4e93df5023ebde
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-09-08 18:40:55 -0500

    Completed int recursion sorting

--- src/com/rohankadkol/Main.java
+++ src/com/rohankadkol/Main.java
@@ -8,7 +8,7 @@ public class Main {
         int[] ints = new int[]{7, 2, 13, 24, 3, 6, 10};
         System.out.println("Before sorting:");
         System.out.println(Arrays.toString(ints));
-        SelectionSort.iterative(ints);
+        SelectionSort.recursive(ints);
         System.out.println("After sorting:");
         System.out.println(Arrays.toString(ints));
     }
--- src/com/rohankadkol/SelectionSort.java
+++ src/com/rohankadkol/SelectionSort.java
@@ -19,4 +19,31 @@ public class SelectionSort {
             items[minIndex] = temp;
         }
     }
+
+    public static void recursive(int[] items) {
+        recursive(items, 0, items.length - 1);
+    }
+
+    private static void recursive(int[] items, int start, int end) {
+        if (start == end) {
+            return;
+        }
+
+        int minIndex = minIndex(items, start + 1, end);
+        if (items[minIndex] < items[start]) {
+            int temp = items[start];
+            items[start] = items[minIndex];
+            items[minIndex] = temp;
+        }
+        recursive(items, start + 1, end);
+    }
+
+    private static int minIndex(int[] items, int start, int end) {
+        if (start == end) {
+            return start;
+        }
+
+        int minIndex = minIndex(items, start + 1, end);
+        return items[start] < items[minIndex] ? start : minIndex;
+    }
 }
